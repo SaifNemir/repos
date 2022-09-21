53b2bcd867e20325cf649067dfd5178884e63e4f
Saif <saif.rashid1974@gmail.com>
2022-09-21 14:22:35 +0200

    saif_2022_09_21

---
+++ MedicalServiceSystem/MedicalServiceSystem/Reports/RPTِEstrdadCountSecLoc.cs
@@ -0,0 +1,27 @@
+namespace MedicalServiceSystem.Reports
+{
+    using System;
+    using System.ComponentModel;
+    using System.Drawing;
+    using System.Windows.Forms;
+    using Telerik.Reporting;
+    using Telerik.Reporting.Drawing;
+
+    /// <summary>
+    /// Summary description for RPTChronicBooksDetails.
+    /// </summary>
+    public partial class RPTöEstrdadCountSecLoc : Telerik.Reporting.Report
+    {
+        public RPTöEstrdadCountSecLoc()
+        {
+            //
+            // Required for telerik Reporting designer support
+            //
+            InitializeComponent();
+
+            //
+            // TODO: Add any constructor code after InitializeComponent call
+            //
+        }
+    }
+}
